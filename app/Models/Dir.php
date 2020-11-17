fa83f9405e7502ec2626977d712baf797e41b96c
kkamara <kelvinkamara@protonmail.com>
2020-11-17 19:55:25 +0000

    Add Dir::class relationship methods

--- app/Models/Dir.php
+++ app/Models/Dir.php
@@ -3,9 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use DirFile;
 
 class Dir extends Model
 {
     use HasFactory;
+
+    /**
+     * Get directory files.
+     * @return Builder
+     */
+    public function files() {
+        return $this->hasMany(DirFile::class);
+    }
+
+    /**
+     * Query scope gets directory files.
+     * @return Builder
+     */
+    public function scopeGetFiles($query) {
+        return $this->has('dir_files');
+    }
 }
