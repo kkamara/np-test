3323e021d65476775a669a03a328d40873913d2b
kkamara <kelvinkamara@protonmail.com>
2020-11-17 22:28:47 +0000

    Form method

--- resources/views/home/show_form.blade.php
+++ resources/views/home/show_form.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
-    <form class='form' action="{{ route('handleForm') }}">
+    <form 
+        class='form' 
+        action="{{ route('handleForm') }}"
+        method='POST'
+    >
+        @csrf
+        @method('POST')
         <label for="search">Submit search:</label>
         <input name='search' type="text"/>
         <input class='btn btn-success' type="submit"/>
