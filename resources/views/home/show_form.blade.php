e17a20a3910ca8d20537bdccdc11ffebf5328b0d
kkamara <kelvinkamara@protonmail.com>
2020-11-17 22:23:19 +0000

    Render form

--- resources/views/home/show_form.blade.php
+++ resources/views/home/show_form.blade.php
@@ -1,1 +1,9 @@
-<p>here</p>
+@extends('layouts.master')
+
+@section('content')
+    <form class='form' action="{{ route('handleForm') }}">
+        <label for="search">Submit search:</label>
+        <input name='search' type="text"/>
+        <input class='btn btn-success' type="submit"/>
+    </form>
+@stop
